b85402921e3e58d8e77454596264d9759b53b4d6
ash-stark-lm <ashish522002@gmail.com>
2024-12-10 01:14:09 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-import { connect } from "mongoose";
+
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server running on port ${process.env.PORT || 4000}`);
+    });
+  })
+  .catch((err) => {
+    console.log(":MongoDB connection FAILED: ", err);
+  });
