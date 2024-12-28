bf337961f7dcb049adcf7f096d03e1591f5966f5
ash-stark-lm <ashish522002@gmail.com>
2024-12-28 17:19:16 +0530

    user register controller setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,77 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Ok",
+  /*
+  i)Get the user data from frontend
+  ii)validation
+  ii)check if user already exists: username, email
+  iv)check for images, check for avatar
+   v)upload them to cloudinary, avatar
+   vi)create user object -> create entry in db
+  vii)remove password and refresh token field from repsonse
+  viii)check for user creation
+  ix)return response
+  */
+
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ userName }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or userName already exists");
+  }
+  //multer gives req.files
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar  file is required");
+  }
+
+  //upload them to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar upload failed");
+  }
+
+  // db entry
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase(),
+    password,
+  });
+
+  //remove password and refresh token field from repsonse
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  //check for user creation
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  //return response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
